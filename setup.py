e2fa6f1df4e1b42328214c0301a8086c04676a26
RaduMolnar99 <radu.molnar@linnify.com>
2020-08-18 17:30:32 +0300

    version 1.2.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='drf-reset-password',
-    version='1.2.6',
+    version='1.2.7',
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     description='Description',
